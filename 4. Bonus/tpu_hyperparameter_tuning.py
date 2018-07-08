d494ccdd35b0ec23f2a1f983e05d4d564f3b1c30
Christian Ramsey <thechristianramsey@gmail.com>
2018-07-08 09:49:14 -0700

    added hyperparameter tuning

---
+++ 4. Bonus/tpu_hyperparameter_tuning.py
@@ -0,0 +1,105 @@
+trainingOutput:
+completedTrialCount: '10'
+consumedMLUnits: 13.08
+isHyperparameterTuningJob: true
+trials:
+- finalMetric:
+    objectiveValue: 0.555806
+    trainingStep: '179'
+  hyperparameters:
+    batchsize: '484'
+    dropout: '0.27851938277724281'
+    feat_eng_cols: 'ON'
+    hidden_units: 64,16
+    learn_rate: '0.082744837373068689'
+  trialId: '9'
+- finalMetric:
+    objectiveValue: 0.539914
+    trainingStep: '214'
+  hyperparameters:
+    batchsize: '404'
+    dropout: '0.23510681204714876'
+    feat_eng_cols: 'ON'
+    hidden_units: 120, 48, 24, 12
+    learn_rate: '0.1225371157647044'
+  trialId: '8'
+- finalMetric:
+    objectiveValue: 0.536858
+    trainingStep: '269'
+  hyperparameters:
+    batchsize: '322'
+    dropout: '0.69860874444556487'
+    feat_eng_cols: 'ON'
+    hidden_units: 64,16
+    learn_rate: '0.077065497802600108'
+  trialId: '1'
+- finalMetric:
+    objectiveValue: 0.524869
+    trainingStep: '251'
+  hyperparameters:
+    batchsize: '345'
+    dropout: '0.33491285352140432'
+    feat_eng_cols: 'ON'
+    hidden_units: 120, 48, 24, 12
+    learn_rate: '0.14031212801135989'
+  trialId: '4'
+- finalMetric:
+    objectiveValue: 0.524051
+    trainingStep: '388'
+  hyperparameters:
+    batchsize: '223'
+    dropout: '0.82573739788902145'
+    feat_eng_cols: 'ON'
+    hidden_units: 64,16
+    learn_rate: '0.082803089054758042'
+  trialId: '5'
+- finalMetric:
+    objectiveValue: 0.519269
+    trainingStep: '290'
+  hyperparameters:
+    batchsize: '298'
+    dropout: '0.5609098059496993'
+    feat_eng_cols: 'ON'
+    hidden_units: 64,16
+    learn_rate: '0.069729717278394762'
+  trialId: '6'
+- finalMetric:
+    objectiveValue: 0.504663
+    trainingStep: '416'
+  hyperparameters:
+    batchsize: '208'
+    dropout: '0.34055125155247806'
+    feat_eng_cols: 'ON'
+    hidden_units: 120, 48, 24, 12
+    learn_rate: '0.16061624206382533'
+  trialId: '7'
+- finalMetric:
+    objectiveValue: 0.452621
+    trainingStep: '596'
+  hyperparameters:
+    batchsize: '145'
+    dropout: '0.86280287384756971'
+    feat_eng_cols: 'ON'
+    hidden_units: 120, 48, 24, 12
+    learn_rate: '0.14704956042580408'
+  trialId: '10'
+- finalMetric:
+    objectiveValue: 0.307978
+    trainingStep: '970'
+  hyperparameters:
+    batchsize: '89'
+    dropout: '0.676460949033334'
+    feat_eng_cols: 'OFF'
+    hidden_units: 64,16,4
+    learn_rate: '0.094045237019706823'
+  trialId: '2'
+- finalMetric:
+    objectiveValue: 0.221321
+    trainingStep: '1629'
+  hyperparameters:
+    batchsize: '53'
+    dropout: '0.59065522991373465'
+    feat_eng_cols: 'ON'
+    hidden_units: 120, 48, 24, 12
+    learn_rate: '0.067561307384850472'
+  trialId: '3'
