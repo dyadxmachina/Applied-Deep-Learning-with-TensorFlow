211e19029d99116ca498b172ec9e1365e158a7af
Christian Fanli Ramsey <thechristianramsey@gmail.com>
2018-07-28 15:54:39 -0700

    added batch instances instead of single

---
+++ 3. Distributed Deep Learning with TensorFlow and Google ML Engine/8 creating your machine learning api/batch_data_dict.py
@@ -0,0 +1,18 @@
+
+requestDict = {
+    'instances': 
+          [
+                {
+                      "Lat": 37.4434774, "Long": -122.1652269, 
+                      "Altitude": 21.0, "Date_": "7/4/17",
+                       "Time_": "23:37:25", "dt_": "7/4/17 23:37" 
+                       #driving meet conjestion
+                },
+                {
+                      "Lat": 37.429302, "Long": -122.16145, 
+                      "Altitude": 20.0, "Date_": "7/4/17",
+                       "Time_": "09:37:25", "dt_": "7/4/17 09:37" 
+                       #driving meet conjestion
+                }
+          ]     
+}
